f2fa9c52876cc66934ea80267bd13e883c375673
MustafaNafizDurukan <37250402+MustafaNafizDurukan@users.noreply.github.com>
2024-06-06 18:12:47 +0300

    Create winget_download.ps1

---
+++ winget_download.ps1
@@ -0,0 +1,12 @@
+try {
+  Install-PackageProvider -Name NuGet -Force | Out-Null
+  Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+} catch {
+  throw "Microsoft.Winget.Client was not installed successfully"
+} finally {
+  # Check to be sure it acutally installed
+  if (-not(Get-Module -ListAvailable -Name Microsoft.Winget.Client)) {
+    throw "Microsoft.Winget.Client was not found. Check that the Windows Package Manager PowerShell module was installed correctly."
+  }
+}
+Repair-WinGetPackageManager
