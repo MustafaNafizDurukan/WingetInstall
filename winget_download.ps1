4bc29de63e57314396b7393b6be816a293d1011a
MustafaNafizDurukan <37250402+MustafaNafizDurukan@users.noreply.github.com>
2024-06-24 12:14:12 +0300

    Update winget_download.ps1

--- winget_download.ps1
+++ winget_download.ps1
@@ -1,12 +1,8 @@
-try {
-  Install-PackageProvider -Name NuGet -Force | Out-Null
-  Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
-} catch {
-  throw "Microsoft.Winget.Client was not installed successfully"
-} finally {
-  # Check to be sure it acutally installed
-  if (-not(Get-Module -ListAvailable -Name Microsoft.Winget.Client)) {
-    throw "Microsoft.Winget.Client was not found. Check that the Windows Package Manager PowerShell module was installed correctly."
-  }
-}
-Repair-WinGetPackageManager
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
